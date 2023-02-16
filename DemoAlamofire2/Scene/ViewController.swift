e7e000c176b1fb9861337815159ffadcc8e21a5e
Piesoft <nut.jirapornpraneet@gmail.com>
2023-02-16 13:51:17 +0700

    add viewmodel

--- DemoAlamofire2/Scene/ViewController.swift
+++ DemoAlamofire2/Scene/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        viewModel.requestCurrency()
     }
 
 
--- DemoAlamofire2/Scene/ViewModel.swift
+++ DemoAlamofire2/Scene/ViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Alamofire
+import AlamofireObjectMapper
+import ObjectMapper
+
+class ViewModel {
+    
+    var info: UserInfo?
+    
+    func requestCurrency(_ completion: ((_ success: Bool) -> ())? = nil) {
+        let request = AF.request("https://swapi.dev/api/people/1/").validate(statusCode:  200..<300)
+        request.responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                guard let castingValue = value as? [String: Any] else { return }
+                guard let data = Mapper<UserInfo>().map(JSON: castingValue) else { return }
+                self.info = data
+                completion?(true)
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion?(false)
+            }
+        }
+    }
+}
