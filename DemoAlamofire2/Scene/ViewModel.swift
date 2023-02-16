bdc05e82e08fcffdae227f39f7caa27fc0e7caa5
Piesoft <nut.jirapornpraneet@gmail.com>
2023-02-16 14:44:03 +0700

    add show data in viewcontroller

--- DemoAlamofire2/Scene/ViewModel.swift
+++ DemoAlamofire2/Scene/ViewModel.swift
@@ -13,9 +13,10 @@ import ObjectMapper
 class ViewModel {
     
     var info: UserInfo?
+    var url = "https://swapi.dev/api/people/1/"
     
     func requestCurrency(_ completion: ((_ success: Bool) -> ())? = nil) {
-        let request = AF.request("https://swapi.dev/api/people/1/", method: .get, parameters: nil, encoding:URLEncoding.default, headers: nil)
+        let request = AF.request(self.url, method: .get, parameters: nil, encoding:URLEncoding.default, headers: nil)
             .validate(statusCode:  200..<300)
             .validate(contentType: ["application/json"])
         request.responseJSON { response in
